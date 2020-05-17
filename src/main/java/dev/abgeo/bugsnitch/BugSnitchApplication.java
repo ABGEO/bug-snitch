3e65798f0dcd5b8bb0cc3cb46af71dbb6ba52fe2
ABGEO <takalandzet@gmail.com>
2020-05-17 15:09:39 +0400

    :tada: Initial commit.

---
+++ src/main/java/dev/abgeo/bugsnitch/BugSnitchApplication.java
@@ -0,0 +1,13 @@
+package dev.abgeo.bugsnitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugSnitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugSnitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abgeo/bugsnitch/BugSnitchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abgeo.bugsnitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugSnitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
